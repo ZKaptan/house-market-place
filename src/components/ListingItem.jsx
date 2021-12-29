8d8fcf3730f9b7fe4924b40cf0c346d2c9355de6
ZKaptan <dkkaptan20@gmail.com>
2021-12-29 16:17:54 -0400

    Can create listings on market place

--- src/components/ListingItem.jsx
+++ src/components/ListingItem.jsx
@@ -4,6 +4,7 @@ import bedIcon from "../assets/svg/bedIcon.svg";
 import bathTubIcon from "../assets/svg/bathtubIcon.svg";
 
 const ListingItem = ({ listing, id, onDelete }) => {
+	console.log(listing);
 	return (
 		<li className="categoryListing">
 			<Link
@@ -11,7 +12,7 @@ const ListingItem = ({ listing, id, onDelete }) => {
 				className="categoryListingLink"
 			>
 				<img
-					src={listing.imageUrls[0]}
+					src={listing.imgUrls[0]}
 					alt={listing.name}
 					className="categoryListingImg"
 				/>
